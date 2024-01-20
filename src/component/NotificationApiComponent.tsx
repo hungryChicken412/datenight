a08a15d0cfe3c8c740871282c2cae978f06ae6b8
Vatsal Shukla <vatsalshukla2001@gmail.com>
2024-01-20 16:34:37 +0530

    jaishriram

--- src/component/NotificationApiComponent.tsx
+++ src/component/NotificationApiComponent.tsx
@@ -6,18 +6,20 @@ import { PopupPosition } from "notificationapi-js-client-sdk/lib/interfaces";
 import { memo, useEffect } from "react";
 
 const NotificationAPIComponent = memo((props: any) => {
-	useEffect(() => {
-		const notificationapi = new NotificationAPI({
-			clientId: "3vs1ibkflgr7ga0eqn4r023k3t",
-			userId: "vatsalshukla2001@gmail.com",
-		});
-		notificationapi.showInApp({
-			root: "CONTAINER_DIV_ID",
-			popupPosition: PopupPosition.BottomLeft,
-		});
-	}, [props.userId]);
+  useEffect(() => {
+    const notificationapi = new NotificationAPI({
+      clientId: "3vs1ibkflgr7ga0eqn4r023k3t",
+      userId: "vatsalshukla2001@gmail.com",
+    });
+    notificationapi.showInApp({
+      root: "CONTAINER_DIV_ID",
+      popupPosition: PopupPosition.BottomLeft,
+    });
+  }, [props.userId]);
 
-	return <div id="CONTAINER_DIV_ID"></div>;
+  return <div id="CONTAINER_DIV_ID"></div>;
 });
+
+NotificationAPIComponent.displayName = 'NotificationAPIComponent';
 
 export default NotificationAPIComponent;
